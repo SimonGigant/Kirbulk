54c56fc85675941a9742efb843ab298404b9e687
Simon <simon.gigant.auditeur@lecnam.net>
2020-09-26 18:10:32 +0200

    :sparkles: SCREENSAKKKKE de marche

---
+++ Assets/Scripts/Controller/ShakeWalk.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeWalk : MonoBehaviour
+{
+    public void Step()
+    {
+        Gamefeel.Instance.InitScreenshake(0.15f, 0.5f);
+    }
+}
