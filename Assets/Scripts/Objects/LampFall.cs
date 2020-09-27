1ffc9f0c1c4294348746eda0122bf9daf3179a86
Cyril <effarek@gmail.com>
2020-09-27 20:59:40 +0200

    ADDED Lamp fall event (à tester ingame). Bizou

---
+++ Assets/Scripts/Objects/LampFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampFall : MonoBehaviour
+{
+    private bool lampHasFallen = false;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!lampHasFallen)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                float zRotation = (collision.gameObject.transform.position.x > transform.parent.position.x ? 80f : -80f );
+
+                transform.parent.localEulerAngles = new Vector3(transform.parent.localEulerAngles.x, transform.parent.localEulerAngles.y, zRotation);
+
+                // A tester in-game hein
+            }
+        }
+    }
+}
