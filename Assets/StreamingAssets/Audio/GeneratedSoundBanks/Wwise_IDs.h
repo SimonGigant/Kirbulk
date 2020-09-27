54a2ba5f85b48e9c3532cb05f546d0d381caa145
Laurent <laurent.bariet.auditeur@lecnam.net>
2020-09-27 18:25:41 +0200

    Wwise New Events

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,24 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_AMB_CENTRE_VILLE = 261130629U;
+        static const AkUniqueID PLAY_AMB_GARDEN_FOREST = 2726245810U;
+        static const AkUniqueID PLAY_AMB_PARC_ENFANT = 2889920656U;
+        static const AkUniqueID PLAY_DOG_CARESSE_HAPPY = 1117641172U;
+        static const AkUniqueID PLAY_DOG_HAPPY = 3743348295U;
+        static const AkUniqueID PLAY_DOG_LEASH = 1142842826U;
+        static const AkUniqueID PLAY_FOOTSTEPS_GRASS = 948914500U;
+        static const AkUniqueID PLAY_FOOTSTEPS_GRASS_SOIL = 896493602U;
+        static const AkUniqueID PLAY_FOOTSTEPS_GRASS_SOIL_HARD = 2659015962U;
+        static const AkUniqueID PLAY_FOOTSTEPS_GRAVEL = 1736684395U;
         static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SFX_BLAST_ZONE_SMASH = 1708987556U;
+        static const AkUniqueID PLAY_SFX_BUMP_TREE = 2514485881U;
+        static const AkUniqueID PLAY_SFX_BUTTERFLY_EXPLOSION = 1348534859U;
+        static const AkUniqueID PLAY_SFX_EXPLOSION = 2514707895U;
+        static const AkUniqueID PLAY_SFX_SMASH_GAME = 3053734711U;
+        static const AkUniqueID PLAY_SFX_SUCCESS_01 = 1064155259U;
+        static const AkUniqueID PLAY_UI_VALIDATION = 745563126U;
     } // namespace EVENTS
 
     namespace BANKS
@@ -24,15 +41,21 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID MASTER_AUDIO = 4157353401U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
         static const AkUniqueID AMBIANCE = 2981377429U;
         static const AkUniqueID FX = 1802970371U;
-        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID VOICE = 3170124113U;
-    } // namespace BUSSES
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
+        static const AkUniqueID CONTROLLER_SPEAKER = 1334442663U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
